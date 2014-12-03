ff69a21f96e1d949fbb8a1afa79861cbe8ee984e
juleskt <julest@bu.edu>
2014-12-02 23:44:37 -0500

    Updated Pic Interpret
    Added function calls to actually call the server.
    Experimental, will probs have to change.

--- FaceOff/src/com/example/faceoff/PictureInterpretation.java
+++ FaceOff/src/com/example/faceoff/PictureInterpretation.java
@@ -6,73 +6,56 @@ import java.util.List;
 import java.util.Map;
 
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.widget.TextView;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.*;
 
 import org.json.*;
 
 public class PictureInterpretation {
 
 	public static ArrayList<Double> Decode(Bitmap picture)
-	{
-		/*This will be the completed ArrayList with X and Y values stored. Formatting is x 0-67, and y 68 - 135*/
+	{    
+		HttpResponse<JsonNode> response = null;
+		
+		class CallMashapeAsync extends AsyncTask<String, Integer, HttpResponse<JsonNode>> 
+		{	
+	    	protected HttpResponse<JsonNode> doInBackground(String... msg) 
+	    	{
+	    		HttpResponse<JsonNode> request = null;
+	    		
+				try 
+				{
+					request = Unirest.post("https://apicloud-facemark.p.mashape.com/process-file.json")
+							.header("X-Mashape-Key", "O5pPl3KTaVmshRGGD5FykeKF31gXp15vSBMjsnfMHFofluIQtP")
+							.field("image", new File("/storage/sdcard0/DCIM/Camera/20141202_212057.jpg"))
+							.asJson();
+				} 
+				catch (UnirestException e) 
+				{
+					e.printStackTrace();
+				}	
+	    		return request;
+	    	}
+	    	protected void onPostExecute(HttpResponse<JsonNode> response) 
+	    	{
+	    		JSONArray Array = response.getBody().getArray();
+	    		String answer = Array.toString();
+	        	System.out.println(answer);
+	        	System.out.println(response.getHeaders());
+	    	}
+		}
+		
+		new CallMashapeAsync().execute();
+		
 		ArrayList<Double> interpretedVals = new ArrayList<Double>();
 		
-		/*These arrays serve as a necessary intermediate step into converting a JSON object into an array. Other advances data structures
-		 * are scary at this point. Maybe replace array architecture with maps/trees in the future*/
-		ArrayList<Double> Xlist = new ArrayList<Double>();
-		ArrayList<Double> Ylist = new ArrayList<Double>();
-		
-		/*Code to convert bmp image to jpg goes here. The API does not support bmp formatting ): */
-		
-		try
-		{
-			/*Attempts to ping the server with the image. If successful, the server should send us back a formatted object
-			 * (in this case, a HttpResponse<JsonNode>. Will want some type of error checking to see if everything was sent well 
-			 * in the future.
-			 * */
-			
-			HttpResponse<JsonNode> response = Unirest.post("https://apicloud-facemark.p.mashape.com/process-file.json")
-				.header("X-Mashape-Key", "jzKWjgvbrgmshp8BRxWzfgKzmBXBp1GvMeVjsnE2ursJJYsuCe")
-				.field("image", new File("<file goes here>"))
-				.asJson();
-			
-			/*Checks the http return status, 200 = OK*/
-			if(response.getStatus() != 200)
-			{
-				System.out.println("Ah... Looks like something went wrong");
-			}
-			
-			/*.getBody() parses the HttpResponse<JsonNode> into a JsonNode object. The JsonNode is converted into a JSONObject via casting
-			 * into Map format*/
-			JSONObject obj = new JSONObject((Map) response.getBody());
-
-			/*Parses the JSONObjects into JSON arrays by looking for the "x" and "y" keys, returning their respective values*/
-			JSONArray arrayX = obj.getJSONArray("x");
-			JSONArray arrayY = obj.getJSONArray("y");
-		
-			for(int i = 0; i < arrayX.length(); i++)
-			{
-				Xlist.add(arrayX.getDouble(i));
-				interpretedVals.add(Xlist.get(i));
-			}
-			for(int j = 0; j < arrayY.length(); j++)
-			{
-				Ylist.add(arrayY.getDouble(j));
-				interpretedVals.add(Ylist.get(j));
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("Error: " + e);
-			e.printStackTrace();
-		}
-		
 		return interpretedVals;
-
 	}
 
 }
