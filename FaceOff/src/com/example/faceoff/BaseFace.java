084ca9b249faf9a638c8f0312e74673d2e8abfea
Brett Moretzky <brettm2277@gmail.com>
2014-12-05 18:39:04 -0500

    Test change 3

--- FaceOff/src/com/example/faceoff/BaseFace.java
+++ FaceOff/src/com/example/faceoff/BaseFace.java
@@ -19,7 +19,7 @@ public class BaseFace extends Activity
 {
 	//What's up?
 	Button camera_button;
-	//Hey
+	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	private Uri fileUri;
 	public static final int MEDIA_TYPE_IMAGE = 1;
