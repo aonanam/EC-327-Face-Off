9dcfbf01ce5d2ca9b0e613f690778ac2111b518c
juleskt <julest@bu.edu>
2014-12-09 01:11:18 -0500

    Merge

--- FaceOff/src/com/example/faceoff/Player2Defense.java
+++ FaceOff/src/com/example/faceoff/Player2Defense.java
@@ -130,9 +130,6 @@ public class Player2Defense extends Activity {
 
 						
 	}
-	
-
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
